5b34487a95bbefe5f212a06109bb4dee323e9a2c
中川-千夏 <116638031+Nakagawa-Nagi@users.noreply.github.com>
2024-12-07 21:24:55 +0800

    ICS I.N.

--- layout/js.js
+++ layout/js.js
@@ -5,12 +5,12 @@ document.writeln("  <script src=\'https://cdn.jsdelivr.net/npm/isotope-layout@3.
 document.writeln("  <script src=\'https://cdn.jsdelivr.net/npm/validate.js@0.13.1/validate.min.js\'></script>");
 document.writeln("  <script src=\'https://cdn.jsdelivr.net/npm/@srexi/purecounterjs@1.5.0/js/purecounter.js\'></script>");
 document.writeln("  <script src=\'https://cdn.jsdelivr.net/npm/swiper@11.1.15/swiper-bundle.min.js\'></script>");
-// document.writeln("  <script src=\'//cdn.icsin.top/live2d_models/lib/L2Dwidget.min.js?094cbace49a39548bed64abff5988b05\'></script>");
-// document.writeln("  <script>L2Dwidget.init({\'pluginRootPath\':\'//cdn.icsin.top/live2d_models/\',\'pluginJsPath\':\'//cdn.icsin.top/live2d_models/lib/\',\'pluginModelPath\':\'//cdn.icsin.top/assets/\',\'tagMode\':false,\'log\':false,\'model\':{\'jsonPath\':\'//cdn.icsin.top/live2d_models/assets/assets/shizuku.model.json\'},\'display\':{\'position\':\'left\',\'width\':150,\'height\':300},\'mobile\':{\'show\':true}});");
+// document.writeln("  <script src=\'../../../../../../live2d_models/lib/L2Dwidget.min.js?094cbace49a39548bed64abff5988b05\'></script>");
+// document.writeln("  <script>L2Dwidget.init({\'pluginRootPath\':\'../../../../../../live2d_models/\',\'pluginJsPath\':\'../../../../../../live2d_models/lib/\',\'pluginModelPath\':\'../../../../../../assets/\',\'tagMode\':false,\'log\':false,\'model\':{\'jsonPath\':\'../../../../../../live2d_models/assets/assets/shizuku.model.json\'},\'display\':{\'position\':\'left\',\'width\':150,\'height\':300},\'mobile\':{\'show\':true}});");
 // document.writeln("  </script>");
-document.writeln("  <script src=\'//cdn.icsin.top/assets/js/main.js\'></script> ");
+document.writeln("  <script src=\'../../../../../../assets/js/main.js\'></script> ");
 document.writeln("  <script type=\'text/javascript\' color=\'79,172,254\' opacity=\'0.7\' zIndex=\'-1\' count=\'200\' src=\'//cdn.bootcss.com/canvas-nest.js/1.0.0/canvas-nest.min.js\'></script>");
 document.writeln("<script src=\'https://cdn.jsdelivr.net/npm/pjax@0.2.8/pjax.min.js\'></script>");
-document.writeln("<script src=\'//cdn.icsin.top/assets/js/pjax.js\'></script>");
+document.writeln("<script src=\'../../../../../../assets/js/pjax.js\'></script>");
 document.writeln("<script src=\'https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.js\'></script>");
 document.writeln("<script src=\'https://cdn.jsdelivr.net/npm/hls.js@1.5.17/dist/hls.min.js\'></script>")
