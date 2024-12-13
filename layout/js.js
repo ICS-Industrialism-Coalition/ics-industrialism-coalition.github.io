7d9b4bfb6820d3c9583c3dc47ecdad802aa22590
中川-千夏 <116638031+Nakagawa-Nagi@users.noreply.github.com>
2024-12-13 23:27:40 +0800

    Fixed URL Error
    Resources
    ICS I.N.
    delete explaination

--- layout/js.js
+++ layout/js.js
@@ -9,7 +9,6 @@ document.writeln("  <script src=\'https://cdn.jsdelivr.net/npm/swiper@11.1.15/sw
 // document.writeln("  <script>L2Dwidget.init({\'pluginRootPath\':\'../../../../../../live2d_models/\',\'pluginJsPath\':\'../../../../../../live2d_models/lib/\',\'pluginModelPath\':\'../../../../../../assets/\',\'tagMode\':false,\'log\':false,\'model\':{\'jsonPath\':\'../../../../../../live2d_models/assets/assets/shizuku.model.json\'},\'display\':{\'position\':\'left\',\'width\':150,\'height\':300},\'mobile\':{\'show\':true}});");
 // document.writeln("  </script>");
 document.writeln("  <script src=\'../../../../../../assets/js/main.js\'></script> ");
-document.writeln("  <script type=\'text/javascript\' color=\'79,172,254\' opacity=\'0.7\' zIndex=\'-1\' count=\'200\' src=\'//cdn.bootcss.com/canvas-nest.js/1.0.0/canvas-nest.min.js\'></script>");
 document.writeln("<script src=\'https://cdn.jsdelivr.net/npm/pjax@0.2.8/pjax.min.js\'></script>");
 document.writeln("<script src=\'../../../../../../assets/js/pjax.js\'></script>");
 document.writeln("<script src=\'https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.js\'></script>");
